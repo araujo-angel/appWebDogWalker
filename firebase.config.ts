555edca745d3858905795771d6e33d02f8a33dcc
Angelica Araujo <angelica.ritart@gmail.com>
2025-02-28 10:29:59 -0300

    criacao do modulo firebase

---
+++ firebase.config.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBiQGbw8b9K2fYX2bQpгAwCi0SjxNEfD0E",
+    authDomain:
+    "social-ifpb-51a5e.firebaseapp.com",
+    projectid:
+    "social-ifpb-51a5e",
+    storageBucket: "social-ifpb-51a5e.firebasestorage.app",
+    messagingSenderId: "971276033393", 
+    appId:
+    "1:971276033393:web:3d66257ef8aa3ec1272ad0"
+    };
